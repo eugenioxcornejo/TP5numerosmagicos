062fc017b04f7f98595833e84f0fb020f99a24e7
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-28 06:35:55 -0300

    ejercicio1 problema guardarvalor onclick

--- js/TP5Ejercicio1.js
+++ js/TP5Ejercicio1.js
@@ -1,9 +1,21 @@
 function numeroaleatorio(min, max) {
-    console.log('nuemro aleatorio generado')
-    min= Math.ceil(min);
-    max= Math.floor(max);
-    return Math.floor(Math.random()*(max-min)+min)
+    console.log('numero aleatorio generado');
+    
+    min= Math.ceil(1);
+    max= Math.floor(100);
+   
+    return Math.floor(Math.random()*(max-min)+min);
+    
 
-}
+};
 
+
+
+enviar = ( ) => {
+    console.log();
+    const numerodelusuario = document.querySelector('#numeroingresado').value;
+   console.log (numerodelusuario)
+    //if 
+//alert()
+};
 
